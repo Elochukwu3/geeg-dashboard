32a33b460b46a3368b4428fae89b63e8c190aabe
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-27 12:14:32 +0100

    update side bar

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex text-black min-h-screen flex-col p-0 m-0 relative">
       <div className="flex h-full min-h-screen">
-        <div className="sticky top-0 left-0 h-full flex max-w-20 w-full  shrink-0 flex-col gap-8">
+        <div className="sticky top-0 left-0 h-full flex max-w-20 w-full  shrink-0 flex-col ">
        <Sidebar/>
         </div>
         <div className="flex-1">
--- components/side-bar/Sidebar.tsx
+++ components/side-bar/Sidebar.tsx
@@ -4,19 +4,23 @@ import Image from "next/image";
 
 function Sidebar() {
   return (
-    <div className="flex py-5   min-h-screen flex-col items-center  gap-5 bg-[#EBECF2]">
-     <div className="flex flex-col items-center  gap-5">
+    <div className="flex py-5 min-h-screen flex-col justify-between  bg-[#EBECF2]">
+     <div className="flex flex-col items-center  gap-2 justify-center">
      {ICON_DATA.slice(0, 8).map(({ icon, name }) => (
-        <span key={`icon${name}`} title={name} className="px-2 cursor-pointer">
+      <div key={`icon${name}`} title={name}  className="px-2 cursor-pointer hover:border-r-2 border-black w-full py-1 flex justify-center">
+        <span  className="px-2 cursor-pointer">
          {icon()}
         </span>
+      </div>
       ))}
      </div>
-      <div className="bg-red-900">
+      <div className="space-y-2">
       {ICON_DATA.slice(8).map(({ icon, name }) => (
-        <span key={`icon${name}`} title={name} className="px-2 cursor-pointer">
-         {icon()}
-        </span>
+         <div key={`icon${name}`} title={name}  className="px-2 cursor-pointer hover:border-r-2 border-black w-full py-1 flex justify-center">
+         <span  className="px-2 cursor-pointer">
+          {icon()}
+         </span>
+       </div>
       ))}
       </div>
     </div>
