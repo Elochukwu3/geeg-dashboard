bba6d39bc1ab9a3e71599f4b906e2cde75fe6c1c
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-27 16:29:05 +0100

    update tailwind style

---
+++ components/style.tsx
@@ -0,0 +1,4 @@
+import tw from "tailwind-styled-components";
+
+export const TreandWrapper = tw.section `py-4 col-span-2 min-h-[374px] h-full px-3 w-full rounded-[14px] border border-[#EDF2F7]  bg-[#fff]`
+export const Trendinner = tw.div`font-jarkata flex justify-between items-center max-w-[766px]`
