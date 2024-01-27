11e8c431494502b275b27018dcbce12abfaa3d3c
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-27 16:28:49 +0100

    update app

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,12 @@
+import Income from "@/components/income/Income";
+import LastOrder from "@/components/last-order/LastOrder";
 import Navbar from "@/components/nav-bar/NavBar";
+import Trend from "@/components/sales-trend/Trend";
+import Sales from "@/components/sales/Sales";
 import Sidebar from "@/components/side-bar/Sidebar";
+import Platform from "@/components/top-platform/Platform";
+import Refund from "@/components/refund/Refund";
+import TotalIncome from "@/components/total-incom/TotalIcome";
 import Image from "next/image";
 
 export default function Home() {
@@ -11,7 +18,19 @@ export default function Home() {
         </div>
         <div className="flex-1">
           <div className="sticky top-0 max-w-[1360px]"><Navbar/></div>
-          <div>content</div>
+          <div className="space-x-4 p-3 grid grid-cols-3 space-y-3">
+            <Trend/>
+            <div className="grid grid-cols-2 gap-x-2">
+            <Income/>
+            <Refund/>
+            <Sales/>
+            <TotalIncome/>
+            </div>
+          </div>
+          <div className="p-3 grid grid-cols-3 space-y-3">
+            <LastOrder/>
+            <Platform/>
+          </div>
         </div>
       </div>
     </main>
