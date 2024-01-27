315c43b3d2a597baf3393ec9ab306a5324147062
Elochukwu3 <oguajuromauld5@gmail.com>
2024-01-27 16:32:13 +0100

    Add icon images

---
+++ utils/db/platFormData.ts
@@ -0,0 +1,6 @@
+export const PlatFormData = [
+    {name: "Book Bazaar", val: 2500000, color: "#6160DC" },
+    {name: "Artisan Aisle", val: 1800000,  color: "#54C5EB"},
+    {name: "Toy Troop", val: 1200000, color: "#FFB74A" },
+    {name: "XStore", val: 900000, color: "#dc6064" },
+] as const;
